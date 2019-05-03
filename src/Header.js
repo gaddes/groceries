2d94ec5a208a5ff7ef7faf3096b078d7a2c15452
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-05-03 08:05:21 -0700

    Rewrite Home and Header as functional components

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className='header'>
-        <Link className='logo' to='/home'>
-          <span className='logo__text -title'>Groceries App</span>
-        </Link>
-      </div>
-    );
-  }
+function Header() {
+  return (
+    <div className='header'>
+      <Link className='logo' to='/home'>
+        <span className='logo__text -title'>Groceries App</span>
+      </Link>
+    </div>
+  );
 }
 
 export default Header;
--- src/Home.js
+++ src/Home.js
@@ -1,55 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './home.scss';
 
-class Home extends Component {
-  render() {
+function Home() {
+  /**
+   * TODO: create event listeners to add functionality to buttons on click
+   */
 
-    /**
-     * TODO: create event listeners to add functionality to buttons on click
-     */
+  /**
+   * TODO: Write function that will be called on 'ADD' button click:
+   *  1) Capture text in input box
+   *  2) Create new <li> element
+   *  3) Text of <li> element should match text content in input box
+   *  4) Append new element to DOM at bottom of current <ul>
+   *  5) Remove text in input element
+   */
 
-    /**
-     * TODO: Write function that will be called on 'ADD' button click:
-     *  1) Capture text in input box
-     *  2) Create new <li> element
-     *  3) Text of <li> element should match text content in input box
-     *  4) Append new element to DOM at bottom of current <ul>
-     *  5) Remove text in input element
-     */
-
-    return (
-      <div className='home'>
-        <div>
-          <h2>Add new item</h2>
-          <div className='flex-row'>
-            <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
-            <button className='item-add'>Add</button>
-          </div>
-        </div>
-          
-        <div className='shopping-list'>
-          <div className='flex-row'>
-            <h2>Shopping list</h2>
-            <button className='item-edit'>Edit</button>
-          </div>
-          <ul className='items'>
-            <li className='item'>
-              <span>Item 1</span>
-              <button className='item-delete'>Delete</button>
-            </li>
-            <li className='item'>
-              <span>Item 2</span>
-              <button className='item-delete'>Delete</button>
-            </li>
-            <li className='item'>
-              <span>Item 3</span>
-              <button className='item-delete'>Delete</button>
-            </li>
-          </ul>
+  return (
+    <div className='home'>
+      <div>
+        <h2>Add new item</h2>
+        <div className='flex-row'>
+          <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
+          <button className='item-add'>Add</button>
         </div>
       </div>
-    );
-  }
+        
+      <div className='shopping-list'>
+        <div className='flex-row'>
+          <h2>Shopping list</h2>
+          <button className='item-edit'>Edit</button>
+        </div>
+        <ul className='items'>
+          <li className='item'>
+            <span>Item 1</span>
+            <button className='item-delete'>Delete</button>
+          </li>
+          <li className='item'>
+            <span>Item 2</span>
+            <button className='item-delete'>Delete</button>
+          </li>
+          <li className='item'>
+            <span>Item 3</span>
+            <button className='item-delete'>Delete</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
