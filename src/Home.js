9c42bdc44eb17deef4aa63aac3ab1aea567a7a29
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-05-03 07:57:31 -0700

    Clean up styling, add edit and delete buttons

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 import './home.scss';
 
 class Home extends Component {
-
-  /**
-   * TODO: Write function that will be called on 'ADD' button click:
-   *  1) Capture text in input box
-   *  2) Create new <li> element
-   *  3) Text of <li> element should match text content in input box
-   *  4) Append new element to DOM at bottom of current <ul>
-   *  5) Remove text in input element
-   */
-
   render() {
+
+    /**
+     * TODO: create event listeners to add functionality to buttons on click
+     */
+
+    /**
+     * TODO: Write function that will be called on 'ADD' button click:
+     *  1) Capture text in input box
+     *  2) Create new <li> element
+     *  3) Text of <li> element should match text content in input box
+     *  4) Append new element to DOM at bottom of current <ul>
+     *  5) Remove text in input element
+     */
+
     return (
-      <div className='body home'>
-        <div className='input-box'>
+      <div className='home'>
+        <div>
           <h2>Add new item</h2>
-          {/* Does this form require method="POST"? */}
-          <form action="/home">
-            <input type="text" placeholder="eggs, chicken, pickles..."></input>
-            <button className='addItem'>Add</button>
-          </form>
+          <div className='flex-row'>
+            <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
+            <button className='item-add'>Add</button>
+          </div>
         </div>
           
         <div className='shopping-list'>
-          <h2>Shopping list</h2>
+          <div className='flex-row'>
+            <h2>Shopping list</h2>
+            <button className='item-edit'>Edit</button>
+          </div>
           <ul className='items'>
-            <li className='item'>Item 1</li>
-            <li className='item'>Item 2</li>
-            <li className='item'>Item 3</li>
+            <li className='item'>
+              <span>Item 1</span>
+              <button className='item-delete'>Delete</button>
+            </li>
+            <li className='item'>
+              <span>Item 2</span>
+              <button className='item-delete'>Delete</button>
+            </li>
+            <li className='item'>
+              <span>Item 3</span>
+              <button className='item-delete'>Delete</button>
+            </li>
           </ul>
         </div>
       </div>
