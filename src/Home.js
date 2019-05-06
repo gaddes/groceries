f22c46f552198c5915b17a186a0438d26406efc8
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-05-06 10:58:54 -0700

    Rewrite ListItems as functional component. Items now held in ShoppingList state and passed to ListItems as props.

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,6 @@ class InputBox extends Component {
   render() {
     return (
       <div>
-        <h2>Add new item</h2>
         <div className='flex-row'>
           <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
           <button className='item-add' onClick={() => this.addItem()}>Add</button>
@@ -27,6 +26,19 @@ class InputBox extends Component {
       </div>
     );
   }
+}
+
+function ListItems(props) {
+  // Loop through items in state, add each one to array as <li>
+  const items = props.items.map((item, index) =>
+    <li key={`item-${index + 1}`}>{item}</li>
+  );
+
+  return (
+    <ul className="list-items">
+      {items}
+    </ul>
+  );
 }
 
 class ShoppingList extends Component {
@@ -60,40 +72,17 @@ class ShoppingList extends Component {
     console.log('item deleted!');
   }
 
-  addItem = () => {
-    // Select input box
-    const inputField = document.querySelector(`.item-input`);
-    const inputContent = inputField.value;
-    // Check not empty
-    if (inputContent !== '') {
-      // Create new array by concatenating existing state with new item
-      const newItems = this.state.items.concat([inputContent]);
-      this.setState({ items: newItems });
-      // Clear input field
-      inputField.value = '';
-    }
-  }
-
   render() {
-    // Loop through items in state, add each one to array as <li>
-    const items = this.state.items.map((item, index) =>
-      <li key={`item-${index + 1}`}>{item}</li>
-    );
-
     return (
       <div className="shopping-list">
         <div className='flex-row'>
           <h2>Shopping list</h2>
           <button className='item-edit'>Edit</button>
         </div>
-        <div className='flex-row'>
-          <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
-          <button className='item-add' onClick={() => this.addItem()}>Add</button>
-        </div>
-        <ul className="list-items">
-          {items}
-        </ul>
-        {/* <button className='item-delete' onClick={() => this.deleteItem()}>Delete</button> */}
+        <InputBox />
+        <ListItems
+          items={this.state.items}
+        />
       </div>
     );
   }
@@ -124,15 +113,6 @@ class Home extends Component {
   render() {
     return (
       <div className='home'>
-        {/* <div>
-          <h2>Add new item</h2>
-          <p>input box to be added back in here...</p>
-          <div className='flex-row'>
-            <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
-            <button className='item-add'>Add</button>
-          </div>
-        </div> */}
-        {/* <InputBox /> */}
         <ShoppingList />
       </div>
     );
