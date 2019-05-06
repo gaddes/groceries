541db029ccc5baa64f4b2b9bd8e82d22013c45dd
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-05-06 10:42:27 -0700

    Commit before abstracting InputBox component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,34 @@
 import React, {Component} from 'react';
 import './home.scss';
 
+class InputBox extends Component {
+  addItem = () => {
+    // Select input box
+    const inputField = document.querySelector(`.item-input`);
+    const inputContent = inputField.value;
+    // Check not empty
+    if (inputContent !== '') {
+      // Create new array by concatenating existing state with new item
+      const newItems = this.state.items.concat([inputContent]);
+      this.setState({ items: newItems });
+      // Clear input field
+      inputField.value = '';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Add new item</h2>
+        <div className='flex-row'>
+          <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
+          <button className='item-add' onClick={() => this.addItem()}>Add</button>
+        </div>
+      </div>
+    );
+  }
+}
+
 class ShoppingList extends Component {
   constructor(props) {
     super(props);
@@ -58,14 +86,14 @@ class ShoppingList extends Component {
           <h2>Shopping list</h2>
           <button className='item-edit'>Edit</button>
         </div>
-        <ul className="list-items">
-          {items}
-        </ul>
-        {/* <button className='item-delete' onClick={() => this.deleteItem()}>Delete</button> */}
         <div className='flex-row'>
           <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
           <button className='item-add' onClick={() => this.addItem()}>Add</button>
         </div>
+        <ul className="list-items">
+          {items}
+        </ul>
+        {/* <button className='item-delete' onClick={() => this.deleteItem()}>Delete</button> */}
       </div>
     );
   }
@@ -96,14 +124,15 @@ class Home extends Component {
   render() {
     return (
       <div className='home'>
-        <div>
+        {/* <div>
           <h2>Add new item</h2>
           <p>input box to be added back in here...</p>
-          {/* <div className='flex-row'>
+          <div className='flex-row'>
             <input className='item-input' type='text' placeholder='eggs, chicken, pickles...'></input>
             <button className='item-add'>Add</button>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
+        {/* <InputBox /> */}
         <ShoppingList />
       </div>
     );
