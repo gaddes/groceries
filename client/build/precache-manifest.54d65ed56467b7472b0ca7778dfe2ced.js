a2eaabfdf7e1fc56bc1ac07fac74619d64eea7f1
Matt Gaddes <mgaddes@absolute.com>
2019-06-30 12:02:46 -0700

    Create build folder with create-react-app

---
+++ client/build/precache-manifest.54d65ed56467b7472b0ca7778dfe2ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84b064dd1660a760fdeaa2fe0072d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c237b951e055f7b0567d",
+    "url": "/static/css/main.7d2539c8.chunk.css"
+  },
+  {
+    "revision": "0fab311fc984006201fa",
+    "url": "/static/js/2.60b31e28.chunk.js"
+  },
+  {
+    "revision": "c237b951e055f7b0567d",
+    "url": "/static/js/main.8a14a2da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
