f1f6e2851d6e11349dc741760cabc03cdc374948
Matthew Gaddes <matthew.gaddes@gmail.com>
2019-07-21 16:42:10 -0700

    Cleanup

--- server/client/src/Item.js
+++ server/client/src/Item.js
@@ -8,25 +8,6 @@ export default function Item(props) {
     return isChecked ? 'checked' : 'unchecked';
   }
 
-  const setChecked = (index) => {
-    console.log(index);
-
-    /**
-     * TODO: Set state of isChecked for specific item
-     * TODO: Do this by providing a click handler as props;
-     * TODO: this click handler will trickle all the way down
-     * TODO: from the ShoppingList component, where state actually lives!
-     * 
-     * TODO: see https://reactjs.org/docs/lifting-state-up.html
-     */
-    
-    // 2) Update textDecoration - how to do this conditionally?
-    // style={{textDecoration: 'line-through'}}
-  
-    // 3) Update isChecked on server
-
-  }
-
   return (
     <div
       className='item flex-row'
